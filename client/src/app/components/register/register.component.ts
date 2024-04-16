8d273b259bbb3e1d38acb7b3e025b290015f06cc
anthonypoh <anthonypoh1998@gmail.com>
2024-04-17 00:39:12 +0800

    register component cleanup

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,13 +1,8 @@
-import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '../../services/user.service';
 import { Router } from '@angular/router';
-import { Store, select } from '@ngrx/store';
-import { selectMessage } from '../../states/selector/message.selector';
-import { Observable, Subscription } from 'rxjs';
 import { MessageService } from 'primeng/api';
-import { CookieService } from 'ngx-cookie-service';
-import { setMessage } from '../../states/action/message.action';
 import { User } from '../../models';
 
 @Component({
@@ -15,35 +10,16 @@ import { User } from '../../models';
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   private router = inject(Router)
   private fb = inject(FormBuilder)
-  private store = inject(Store)
   private userService = inject(UserService)
   private messageService = inject(MessageService)
-  private cookieService = inject(CookieService)
 
   form!: FormGroup
 
-  message$!: Observable<string>
-  message: string = ''
-  messageSubscription!: Subscription;
-
   ngOnInit(): void {
     this.createForm()
-
-    this.message$ = this.store.pipe(
-      select(selectMessage)
-    )
-    this.messageSubscription = this.message$.subscribe(message => {
-      switch (message) {
-        case "Incorrect username or password.":
-          this.addSingle('warn', message)
-          break;
-        default:
-          break;
-      }
-    });
   }
 
   createForm() {
@@ -60,17 +36,12 @@ export class RegisterComponent {
     this.userService.register(user)
       .then(msg => {
         console.log(msg)
-        this.router.navigate(['#'])
+        if (msg == 'success') {
+          this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Registration complete' });
+          this.router.navigate(['#'])
+        } else {
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Something went wrong' });
+        }
       })
-  }
-
-  addSingle(severity: string, msg: string) {
-    this.message = msg
-    this.messageService.add({ severity: severity, summary: this.message });
-    this.store.dispatch(setMessage({ message: '' }))
-  }
-
-  ngOnDestroy() {
-    this.messageSubscription.unsubscribe();
   }
 }
